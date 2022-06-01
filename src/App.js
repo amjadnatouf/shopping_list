cb7355565a1fdae4df53811745308a2b9c5fb6a9
amjad <na80@hotmail.com>
2022-06-01 13:17:10 +0200

    uploading-files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import List from "./List";
+import "./App.css";
+import Modal from "./modal/Modal";
 
-function App() {
+const App = () => {
+  const [list, setList] = useState([]);
+  const [name, setName] = useState("");
+  const [edit, setEdit] = useState("");
+  const [show, setShow] = useState(false);
+  const [id, setId] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      console.log("pleas enter a value");
+    } else {
+      const newItem = {
+        id: new Date().getTime().toString(),
+        title: name,
+      };
+      setList([...list, newItem]);
+      setName("");
+    }
+  };
+
+  const removeItem = (id) => {
+    setList(list.filter((item) => item.id !== id));
+  };
+
+  const editItem = (id) => {
+    setId(id);
+    showModal();
+  };
+
+  const updateItem = () => {
+    if (edit !== "") {
+      setList(
+        list.map((item) => (item.id === id ? { ...item, title: edit } : item))
+      );
+    }
+    setShow(false);
+  };
+
+  const clearList = () => {
+    setList([]);
+  };
+
+  const showModal = function () {
+    setShow(!show);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Modal
+        show={show}
+        edit={edit}
+        setEdit={setEdit}
+        updateItem={updateItem}
+        showModal={showModal}
+      />
+      <section className="section-center">
+        <form className="regform">
+          <h3> shopping list </h3>
+          <div className="form-control">
+            <input
+              type="text"
+              className="input-control"
+              placeholder="e.g. eggs"
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
+            <button className="btn-add" onClick={handleSubmit}>
+              submit
+            </button>
+          </div>
+        </form>
+
+        <div className="items-container">
+          <List
+            items={list}
+            removeItem={removeItem}
+            editItem={editItem}
+            showModal={showModal}
+          />
+          <button className="btn-danger" onClick={clearList}>
+            Clear Items
+          </button>
+        </div>
+      </section>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/modal/Modal.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./modal.css";
+
+const Modal = ({ show, edit, setEdit, updateItem, showModal }) => {
+  return (
+    <div>
+      {show && (
+        <div className="layer" onClick={showModal}>
+          asd
+        </div>
+      )}
+      {show && (
+        <div className="modal-container">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h3>Edit Item</h3>
+              <button className="btn-close" onClick={showModal}>
+                x
+              </button>
+            </div>
+            <div className="modal-body">
+              <input
+                type="text"
+                name="groceryTitle"
+                value={edit}
+                onChange={(e) => setEdit(e.target.value)}
+              />
+            </div>
+            <div className="modal-footer">
+              <button className="btn-danger" onClick={showModal}>
+                Close
+              </button>
+              <button className="btn-edit" onClick={updateItem}>
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Modal;
